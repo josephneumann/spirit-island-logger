d01cf462ab66118ed605716e5ba287b60ff38c7b
Joseph Neumann <jneumann@aledade.com>
2020-04-24 09:09:46 -0500

    Simplify game history on index page.  Create game history dedicated page

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ from app.forms import CreateGameForm, EditGameForm, ScoreGameForm, RandomizeGame
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index", methods=["GET", "POST"])
 def index():
-    games = Game.query.order_by(Game.id.desc()).all()
+    games = Game.query.order_by(Game.date.desc()).limit(5)  # Only Retrieve Latest 5 Games
     form = CreateGameForm()
     form.expansions.choices = [
         (e.id, e.name) for e in Expansion.query.filter(Expansion.id != "base").all()
@@ -29,9 +29,15 @@ def index():
         db.session.commit()
         return redirect(url_for("edit_game", game_id=new_game.id))
     for error in form.errors:
-        flash("{}: {}".format(error.capitalize(),form.errors[error][0]), "danger")
+        flash("{}: {}".format(error.capitalize(), form.errors[error][0]), "danger")
 
     return render_template("index.html", title="Home", games=games, form=form)
+
+
+@app.route("/games")
+def games():
+    games = Game.query.order_by(Game.id.desc()).all()
+    return render_template("games.html", title="Home", games=games)
 
 
 @app.route("/game/<game_id>/edit", methods=["GET", "POST"])
@@ -115,7 +121,7 @@ def edit_game(game_id):
         form.notes.data = gm.notes
 
     for error in form.errors:
-        flash("{}: {}".format(error.capitalize(),form.errors[error][0]), "danger")
+        flash("{}: {}".format(error.capitalize(), form.errors[error][0]), "danger")
     return render_template("edit_game.html", title="Edit Game", game=gm, form=form)
 
 
@@ -140,13 +146,11 @@ def score_game(game_id):
         db.session.add(gm)
         db.session.commit()
 
-        flash("Game score: {}".format(gm.score),"success")
+        flash("Game score: {}".format(gm.score), "success")
         return redirect(url_for("edit_game", game_id=gm.id))
     for error in form.errors:
-        flash("{}: {}".format(error.capitalize(),form.errors[error][0]), "danger")
-    return render_template(
-        "score_game.html", title="Score Game", game=gm, form=form
-    )
+        flash("{}: {}".format(error.capitalize(), form.errors[error][0]), "danger")
+    return render_template("score_game.html", title="Score Game", game=gm, form=form)
 
 
 @app.route("/game/<game_id>/delete")
@@ -182,9 +186,11 @@ def randomize_game(game_id):
         db.session.add(gm)
         db.session.commit()
         if form.force.data:
-            flash("Game randomized - existing game configuration overwritten","success")
+            flash(
+                "Game randomized - existing game configuration overwritten", "success"
+            )
         else:
-            flash("Game randomized - existing game configuration conserved","success")
+            flash("Game randomized - existing game configuration conserved", "success")
         return redirect(url_for("edit_game", game_id=gm.id))
     elif request.method == "GET":
         form.use_thematic_boards.data = gm.is_thematic if gm.boards else True
@@ -194,5 +200,5 @@ def randomize_game(game_id):
         form.use_adversary.data = False
         form.adversary_max_difficulty.data = "10"
     for error in form.errors:
-        flash("{}: {}".format(error.capitalize(),form.errors[error][0]), "danger")
+        flash("{}: {}".format(error.capitalize(), form.errors[error][0]), "danger")
     return render_template("randomize_game.html", game=gm, form=form)
